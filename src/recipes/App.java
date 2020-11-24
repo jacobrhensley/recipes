b5a6db36839339f33cb4cb055d669e36c195156e
Jacob Hensley <jacobrhensley3@gmail.com>
2020-11-24 15:08:37 -0600

    created showRecipe method and tested functionallity with demo recipe

--- src/recipes/App.java
+++ src/recipes/App.java
@@ -1,11 +1,10 @@
 package recipes;
 
-import java.util.Arrays;
-
 public class App {
     public static void main(String[] args) {
 
         Recipe PBandJ = new Recipe();
+
 
         PBandJ.title = "PB&J";
         String[] i = {"Peanut Butter", "Jelly", "Bread"};
@@ -13,13 +12,23 @@ public class App {
         String[] ins = {"Spread Peanut Butter on 1 slice of Bread", "Spread Jelly on 2nd slice of Bread", "Place 2 pieces of bread together where the Peanut Butter and Jelly touch", "Enjoy"};
         PBandJ.instructions = ins;
 
-        System.out.println(PBandJ.title);
+        showRecipe(PBandJ);
 
-        for(int x = 0; x<PBandJ.ingredients.length; x++){
-            System.out.println(PBandJ.ingredients[x]);
+    }
+
+    public static void showRecipe(Recipe recipe) {
+        System.out.println("-------------------------------------------------------------------------------------------------------");
+        System.out.println("Title: " + recipe.title);
+        System.out.println("-----------");
+        System.out.println("Ingredients:");
+        for(int x = 0; x<recipe.ingredients.length; x++){
+            System.out.println(recipe.ingredients[x]);
         }
-        // System.out.println(Arrays.toString(PBandJ.ingredients));
-
-
+        System.out.println("-----------");
+        System.out.println("Instructions:");
+        for(int x = 0; x<recipe.instructions.length; x++){
+            System.out.println(x + 1 + ": " + recipe.instructions[x]);
+        }
+        System.out.println("-------------------------------------------------------------------------------------------------------");
     }
 }
